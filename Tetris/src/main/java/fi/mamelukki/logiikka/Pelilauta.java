38facd49eb78dfb5152b2fe08e34fe80956d98d1
Mamelukki <salmisar@cs.helsinki.fi>
2017-02-03 21:58:26 +0200

    uudet luokat

---
+++ Tetris/src/main/java/fi/mamelukki/logiikka/Pelilauta.java
@@ -0,0 +1,236 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fi.mamelukki.logiikka;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.util.Random;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author salmisar
+ */
+public class Pelilauta extends JPanel {
+
+    public static Tetromino pala;
+    public static int seuraavaPala;
+    private static int pelilauta[][];
+    private static int y;
+    private static int x;
+    private static Random random = new Random();
+    
+    // Sisältää oleellisen pelilogiikan, esimerkiksi rivien tyhjentämisen ja uuden palan luomisen
+
+    public Pelilauta() { // Luo pelilautan ja ensimmäisen tetrominon
+        this.setPreferredSize(new Dimension(200, 400));
+        this.pelilauta = new int[21][12];
+        for (int i = 0; i < 21; i++) // Pelilaudan koko on 10 x 20, reunoilla on yhdeksikköjä
+        {
+            this.pelilauta[i][11] = this.pelilauta[i][0] = 9;
+        }
+        for (int i = 0; i < 12; i++) {
+            this.pelilauta[20][i] = 9;
+        }
+        this.y = 1;
+        this.x = 4;
+        this.pala = new Tetromino();
+        this.seuraavaPala = this.random.nextInt(7); // Arvotaan seuraava pala
+    }
+
+    @Override
+    public void paint(Graphics g) { // Piirtää pelilaudan ja sen elementit
+        super.paint(g);
+        for (int y = 0; y < 20; y++) {
+            for (int x = 1; x < 11; x++) {
+                if (this.pelilauta[y][x] != 0) {
+                    g.setColor(Color.black);
+                    g.fillRect((x - 1) * 20, y * 20, 20, 20);
+                    g.setColor(new Color((this.pelilauta[y][x]) * 500 + 1));
+                    g.fillRect(20 * (x - 1) + 1, 20 * y + 1, 18, 18);
+                }
+            }
+        }
+    }
+
+    public int[][] getPelilauta() {
+        return this.pelilauta;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public void setY(int arvo) {
+        this.y = arvo;
+    }
+
+    public void setX(int value) {
+        this.x = value;
+    }
+
+    public static int getSeuraavaMuoto() {
+        return seuraavaPala;
+    }
+
+    public void merkitseTetromino() { // Merkitsee aktiivisen tetrominon pelilaudalle
+        int[][] temp = this.pala.getTetromino();
+        int muoto = this.pala.getMuoto();
+
+        if (muoto == 7) { // Jos tetromino on tyhja, ei tarvitse tehdä mitään
+            return;
+        }
+        for (int i = 0; i < 4; i++) {
+            this.pelilauta[this.y + temp[i][1]][this.x + temp[i][0]] = muoto + 1;
+        }
+    }
+
+    public void poistaTetromino() { // Poistaa aktiivisen tetrominon pelilaudalta
+        int[][] temp = this.pala.getTetromino();
+        for (int i = 0; i < 4; i++) {
+            this.pelilauta[this.y + temp[i][1]][this.x + temp[i][0]] = 0;
+        }
+    }
+
+    public boolean onkoTilaa(int[][] uudetPisteet, int tarkastettavaX, int tarkastettavaY) { // Tarkistaa onko pelilaudalla tilaa tetrominolle
+        boolean tulos = true;
+        poistaTetromino();
+        
+        for (int i = 0; i < 4; i++) {
+            if (this.pelilauta[tarkastettavaY + uudetPisteet[i][1]][tarkastettavaX + uudetPisteet[i][0]] != 0) {
+                tulos = false;
+            }
+        }
+        
+        merkitseTetromino();
+        return tulos;
+    }
+
+    public Tetromino siirraAlas() { // Siirtää tetrominon yhden "pykälän" alemmas
+        if (this.pala == null) {
+            return this.pala;
+        }
+        if (this.onkoTilaa(this.pala.getTetromino(), this.x, this.y + 1)) {
+            poistaTetromino();
+            this.y++;
+            merkitseTetromino();
+            repaint();
+            return this.pala;
+        } else {
+            repaint();
+            return vapautaTetromino();
+        }
+    }
+
+    public Tetromino siirraVasemmalle() { // Siirtää tetrominon vasemmalle
+        if (this.pala == null) {
+            return this.pala;
+        }
+        if (this.onkoTilaa(this.pala.getTetromino(), this.x - 1, this.y)) {
+            poistaTetromino();
+            this.x--;
+            merkitseTetromino();
+            repaint();
+            return this.pala;
+        } else {
+            return this.pala;
+        }
+    }
+
+    public Tetromino siirraOikealle() { // Siirtää tetrominon oikealle 
+        if (this.pala == null) {
+            return this.pala;
+        }
+        if (this.onkoTilaa(this.pala.getTetromino(), this.x + 1, this.y)) {
+            poistaTetromino();
+            this.x++;
+            merkitseTetromino();
+            repaint();
+            return this.pala;
+        } else {
+            return this.pala;
+        }
+
+    }
+
+    public void kaanna() { // Kääntää tetrominon
+        if (this.onkoTilaa(this.pala.seuraavaKaannos(), this.x, this.y)) { // Tarkistaa onko kääntämiseen tilaa
+            poistaTetromino();
+            this.pala.kaanna();
+            merkitseTetromino();
+            repaint();
+        }
+    }
+
+    public String tulostaPelilauta() { // Tulostaa pelilaudan merkkijonona
+        String tulos = "";
+        for (int i = 0; i < 20; i++) {
+            for (int j = 1; j < 11; j++) {
+                tulos += this.pelilauta[i][j];
+            }
+            tulos += "\n";
+        }
+        return tulos;
+    }
+
+    private Tetromino vapautaTetromino() { // Vapauttaa tetrominon ja luo uuden
+        tarkistaRivit();
+        this.y = 1;
+        this.x = 4;
+        this.pala = new Tetromino(seuraavaPala);
+        this.seuraavaPala = random.nextInt(7);
+        return this.pala;
+    }
+
+    private void tarkistaRivit() { // Tarkistaa täydet rivit
+        int[] taydetRivit = new int[4];
+        int indeksi = 0;
+        for (int i = 0; i < 20; i++) {
+            for (int j = 1; j < 11; j++) {
+                if (this.pelilauta[i][j] == 0) {
+                    break;
+                }
+                if (j == 10) {
+                    taydetRivit[indeksi] = i;
+                    indeksi++;
+                }
+            }
+        }
+        tyhjennaRivit(taydetRivit);
+    }
+
+    private void tyhjennaRivit(int[] taydetRivit) { // Poistaa täytetyt rivit
+        for (int i = 0; i < 4; i++) {
+            if (taydetRivit[i] != 0) {
+                for (int j = 1; j < 11; j++) {
+                    this.pelilauta[taydetRivit[i]][j] = 0;
+                }
+            }
+        }
+        repaint();
+        pudotaRivit(taydetRivit);
+    }
+
+    private void pudotaRivit(int[] taydetRivit) { // Pudottaa kaikki mahdolliset rivit alaspäin sen jälkeen, kun jokin rivi on tyhjennetty
+        int valmiitRivit = 0;
+        for (int i = 0; i < 4; i++) {
+            if (taydetRivit[i] != 0) {
+                valmiitRivit++;
+                for (int j = taydetRivit[i]; j > 0; j--) {
+                    for (int k = 1; k < 11; k++) {
+                        this.pelilauta[j][k] = this.pelilauta[j - 1][k];
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ Tetris/src/main/java/fi/mamelukki/logiikka/Tetromino.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fi.mamelukki.logiikka;
+
+import java.util.Random;
+
+/**
+ *
+ * @author salmisar
+ */
+public class Tetromino {
+
+    private int[][] tetromino;
+    private int muoto;
+    private static int[][][] tetrominot // Taulukko, joka sisältää eri muotoisten Tetrominojen koordinaatit
+            = 
+            // IPala (0)
+            {{{0, 0}, {0, 1}, {0, 2}, {0, -1}},
+            // KuutioPala (1)
+            {{0, 0}, {0, 1}, {1, 0}, {1, 1}},
+            // SPala (2)
+            {{0, 0}, {1, 0}, {1, 1}, {0, -1}},
+            // ZPala (3)
+            {{0, 0}, {1, 0}, {0, 1}, {1, -1}},
+            // TPala (4)
+            {{0, 0}, {1, 0}, {0, 1}, {-1, 0}},
+            // LPala (5)
+            {{0, 0}, {0, -1}, {0, 1}, {1, -1}},
+            // JPala (6)
+            {{0, 0}, {0, -1}, {0, 1}, {-1, -1}},
+            // TyhjaPala (7)
+            {{0, 0}, {0, 0}, {0, 0}, {0, 0}}};
+
+    public Tetromino() {
+        this.tetromino = new int[4][2];
+        this.muoto = satunnainenMuoto();
+        this.tetromino = tetrominot[this.muoto];
+    }
+
+    public Tetromino(int muoto) {
+        tetromino = new int[4][2];
+        this.muoto = muoto;
+        tetromino = tetrominot[this.muoto];
+    }
+
+    public void kaanna() { // Päivittää tetrominotaulukon aina kunkin kaannoksen jalkeen
+        this.tetromino = this.seuraavaKaannos();
+    }
+
+    public int[][] seuraavaKaannos() { // Päivittää taulukon vastaamaan tetrominon seuraavaa käännöstä
+        if (this.muoto == 1) { // Jos tetromino on kuutio, palaa ei tarvitse kääntää
+            return this.tetromino;
+        }
+
+        int[][] tulos = new int[4][2];
+        for (int i = 0; i < 4; i++) {
+            tulos[i][0] = this.tetromino[i][1];
+            tulos[i][1] = -this.tetromino[i][0];
+        }
+
+        return tulos;
+    }
+
+    public int[][] getTetromino() {
+        return this.tetromino;
+    }
+
+    public static int[][] getTetromino(int muoto) {
+        return tetrominot[muoto];
+    }
+
+    public int getMuoto() {
+        return this.muoto;
+    }
+
+    private int satunnainenMuoto() { //  Arpoo jonkin seitsemästä tetromino-muodosta
+        Random muodonArpoja = new Random();
+        return muodonArpoja.nextInt(7);
+    }
+}
